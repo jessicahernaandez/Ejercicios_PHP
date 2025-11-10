795c4d23f2dc5c95ffe3931dd9a1a2c6ba7a2d6c
Jessica Hernandez <jessicahernaandez23@gmail.com>
2025-11-10 10:04:44 +0100

    Ejercicio 1 empezado

---
+++ Funciones/Ejercicio1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funciones</title>
+</head>
+<body>
+    <?php 
+    
+        include("arrayPar.php");
+
+        echo esPar(5);
+    
+    ?>
+</body>
+</html>
---
+++ Funciones/arrayPar.php
@@ -0,0 +1,40 @@
+<?php 
+
+    // Funcion que averigue si un número es par
+    function esPar(int $intNum) : bool {
+        
+        $esPar = $intNum % 2 == 0; true : false;
+
+        return $esPar;
+    }
+
+    // Una función que devuelva un array de tamaño $intTam con números aleatorios 
+    // comprendido entre $intMin y $intMax.
+    function arrayAleatorio (int $intTam, int $intMin, int $intMax) : array {
+        $numAleatorios = [];
+
+        for($intCont=0;$intCont<=$intTam;$intCont++) {
+            $numAleatorios[$intCont] = rand($intMin,$intMax);
+        }
+
+        return $numAleatorios;
+    }
+
+    // Una función que reciba un $array por referencia y devuelva la cantidad de números 
+    // pares que hay almacenados: arrayPares(array &$array): int
+    function arrayPares (array &$array) : int {
+
+        $intCantidadPares = 0;
+        for($intCont=0;$intCont<=count($array);$intCont++) {
+            if($array[$intCont] % 2 == 0) {
+                $intCantidadPares++;
+            }
+        }
+
+        return $intCantidadPares;
+    }
+
+
+
+
+?>
