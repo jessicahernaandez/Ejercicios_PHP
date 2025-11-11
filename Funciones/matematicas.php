1d24e33da68d707c32a30da582e2b8a48da67afc
Jessica Hernandez <jessicahernaandez23@gmail.com>
2025-11-11 09:27:27 +0100

    Siguiendo con versiones, prueba de cambio de version de xampp

--- Funciones/matematicas.php
+++ Funciones/matematicas.php
@@ -7,6 +7,17 @@
         return $numDigitos;
     }
 
+    // digitoN(int $num, int $pos): int → devuelve el dígito que ocupa, empezando por la izquierda, la posición $pos.
+    function digitoN (int $num, int $pos) : int {
+
+        $arrNum = (array) $num;
+        print_r($arrNum);
+        $pos= 0;
+
+        return $pos;
+    }
+
+    echo digitoN(12856, 1);
 
 
 ?>
